607d70155602d00833497542ddb75eb389025e99
progyanedu <159709924+progyanedu@users.noreply.github.com>
2024-03-10 21:00:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Switch to : <a href="https://www.progyaneducations.in"> Progyan Educations</a></h1>
+    </div>
+  );
+}
+
+export default App;
+
+// Path: prog-live/src/index.js
