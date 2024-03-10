a6da74b9380beaa7d904b35cb29a9c651919d682
progyanedu <159709924+progyanedu@users.noreply.github.com>
2024-03-10 22:31:12 +0530

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Switch to : <a href="https://www.progyaneducations.in"> Progyan Educations</a></h1>
+      <h1>Switch to : <a href="https://www.progyaneducations.in"> Progyan Educations.in</a></h1>
     </div>
   );
 }
